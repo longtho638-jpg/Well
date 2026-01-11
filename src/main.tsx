47d927d76f691404ed0cd95b3fa43085d13df1e2
Người hướng dẫn <longtho638@gmail.com>
2026-01-11 14:44:34 +0700

    fix: Remove v7 future flags from BrowserRouter to fix navigation

--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
       <LanguageProvider>
-        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </LanguageProvider>
